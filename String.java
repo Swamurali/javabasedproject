dad1500a0a11d2a43e3fce7d83428c0137bfe154
swathi <swamurali@gmail.com>
2024-02-13 12:57:29 +0000

    String.java has been added

---
+++ String.java
@@ -0,0 +1,11 @@
+class String{
+public static void main (String[] args){
+
+String name = "Swathi";
+String profile = "cloud engineer";
+
+System.out.println(name);
+System.out.println(profile);
+
+}
+}
